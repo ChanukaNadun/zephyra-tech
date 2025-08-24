89da5e7bfc0e229b6a81d980e91254d3ff8b7e92
ChanukaNadun <chanukanadun07@gmail.com>
2025-08-24 07:48:43 +0530

    change page route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import toast from "react-hot-toast";
-import SignInPage from "./sign-in";
+import SignInPage from "./sign-in/page";
 
 export default function Home() {
   return (
-   <SignInPage/>
+    <div>
+      <SignInPage />
+    </div>
   );
 }
